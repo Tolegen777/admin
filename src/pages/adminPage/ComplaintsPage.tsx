c15ebcbbfb55fc55e3a417f6541767fa32d0379e
unknown <dico.bb15@mail.ru>
2022-05-04 17:36:49 +0600

    some changes

--- src/pages/adminPage/ComplaintsPage.tsx
+++ src/pages/adminPage/ComplaintsPage.tsx
@@ -1,25 +1,22 @@
-import React, {Suspense} from 'react';
-import {Box, Paper, Stack, Typography} from "@mui/material";
-import {Route, Routes} from "react-router-dom";
-import ComplaintsList from '../../components/pages/complaint/ComplaintsList';
-
+import React, { Suspense } from "react";
+import { Box, Paper, Stack, Typography } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import ComplaintsList from "../../components/pages/complaint/ComplaintsList";
 
 const ComplaintsPage = () => {
-    return (
-        <Box>
+  return (
+    <Box>
+      <Stack>
+        <Typography variant={"h3"}>Список жалоб</Typography>
+      </Stack>
 
-                <Stack>
-                    <Typography>Список жалоб</Typography>
-                </Stack>
-
-            <Suspense fallback={<div>Загрузка...</div>}>
-                    <Routes>
-                        <Route index element={<ComplaintsList />} />
-                    </Routes>
-
-            </Suspense>
-        </Box>
-    );
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route index element={<ComplaintsList />} />
+        </Routes>
+      </Suspense>
+    </Box>
+  );
 };
 
 export default ComplaintsPage;
