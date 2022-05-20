59dc3c8adcbc4e5d21f256b69bc91ca5dfb264eb
Tolegen777 <tolegenmukan@MacBook-Air-Tolegen.local>
2022-05-20 17:07:27 +0600

    fixing

--- src/components/drawer/DrawerAdmin.tsx
+++ src/components/drawer/DrawerAdmin.tsx
@@ -119,12 +119,8 @@ const DrawerAdmin = () => {
         <List>
           <StyledNavLink to="">
             <StyledListItem
-<<<<<<< HEAD
                 // @ts-ignore
-=======
-              // @ts-ignore
->>>>>>> 92b100f1c76e12fdd5b92e5e645128dbf2b3100e
-              onClick={() => dispatch(logout())}
+                onClick={() => dispatch(logout())}
               sx={{ color: "#F18989", mb: "35px" }}
             >
               <StyledListItemIcon>
